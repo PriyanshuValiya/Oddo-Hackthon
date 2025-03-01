513c319c31beaccc68b3a9c3fddd5d8845bd0f26
veer <patel.veer2110@gmail.com>
2025-03-01 11:29:14 +0530

    singIn and signUp form formating

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,8 +2,15 @@ import { SignIn } from "@clerk/nextjs";
 
 function Page() {
   return (
-    <div><SignIn /></div>
-  )
+    <div 
+      className="flex justify-center items-center min-h-screen bg-cover bg-center" 
+      style={{ backgroundImage: "url('https://images.pexels.com/photos/5466793/pexels-photo-5466793.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}
+    >
+      <div className="">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
 
 export default Page;
--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -2,8 +2,15 @@ import { SignUp } from "@clerk/nextjs";
 
 function Page() {
   return (
-    <div><SignUp /></div>
-  )
+    <div 
+      className="flex justify-center items-center min-h-screen bg-cover bg-center" 
+      style={{ backgroundImage: "url('https://images.pexels.com/photos/5466793/pexels-photo-5466793.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}
+    >
+      <div className="">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
 
 export default Page;
